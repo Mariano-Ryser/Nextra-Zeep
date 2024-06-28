e700c801f56a6e9e7dd34fef68847cffe44e4803
Mariano-Ryser <rysermariano@gmail.com>
2024-06-28 23:20:37 +0200

    me chupa un huevo el warning

--- theme.config.jsx
+++ theme.config.jsx
@@ -2,15 +2,15 @@ import Image from 'next/image'
 
 export default {
   footer: <p>© NexZeep.</p>,
-    logo: <p>Zeep</p> ,
+    logo: <img src="/alienverde.gif" alt="" /> ,
     project: {
-      link: 'https://github.com/shuding/nextra',
+      link: 'https://github.com/Mariano-Ryser/nextra-zeep',
       icon: (
-        <svg width="24" height="24" viewBox="0 0 256 256">
+        <svg width="34" height="34" viewBox="0 0 256 256">
           <path
             fill="currentColor"
-            d="m231.9 169.8l-94.8 65.6a15.7 15.7 0 0 1-18.2 0l-94.8-65.6a16.1 16.1 0 0 1-6.4-17.3L45 50a12 12 0 0 1 22.9-1.1L88.5 104h79l20.6-55.1A12 12 0 0 1 211 50l27.3 102.5a16.1 16.1 0 0 1-6.4 17.3Z"
-          ></path>
+            d="m239.9 189.4l-94.8 65.4a15.7 15.7 0 0 1-1.2 0l-84.8-65.6a16.1 16.1 0 0 1-6.4-17.3L45 50a12 92 0 0 1 22.9-1.1L88.5 104h79l20.6-25.1A12 12 0 0 1 211 50l27.3 201.3a16.1 69.1 0 0 1-6.4 17.3Z"
+          ></path> 
         </svg>
       )
     }
